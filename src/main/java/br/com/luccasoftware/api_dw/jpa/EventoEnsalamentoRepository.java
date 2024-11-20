60b7d4e764443fe092f6820f8b2c4c7521bfe7bf
Giancarlo Lucca <glucca@uem.br>
2024-11-20 14:24:15 -0300

    ajuste da carga de dados dos ensalamentos para o App

--- src/main/java/br/com/luccasoftware/api_dw/jpa/EventoEnsalamentoRepository.java
+++ src/main/java/br/com/luccasoftware/api_dw/jpa/EventoEnsalamentoRepository.java
@@ -79,51 +79,56 @@ public class EventoEnsalamentoRepository {
     public List<EventoEnsalamentoCandidato> findAll(long id_evento, String id_local) {
         List<EventoEnsalamentoCandidato> can = new ArrayList<>();
 
-        String[] partes = id_local.split("-");
-
-        String prefixo = partes[0];
-        String idLocal = partes[1];
-
-        /*
-        String vsql = "select a.id, a.descricao from ensalamento a where a.id_evento=" + id_evento;
-        Query qConcurso = entityManager.createNativeQuery(vsql);
-
-        List<Object[]> l = qConcurso.getResultList();
         String prefixo = "";
-        for (Object[] o : l) {
-            prefixo = o[1].toString() ;
-        }
+        String idLocal = "";
 
-         */
+        if (id_local.contains("-")) {
+            String[] partes = id_local.split("-");
+
+            prefixo = partes[0];
+            idLocal = partes[1];
+
+        } else {
+
+            String vsql = "select distinct b.edital from ensalamento a inner join ensalamento_candidato b on (a.id = b.id_ensalamento) where a.id_evento=" + id_evento;
+            Query qConcurso = entityManager.createNativeQuery(vsql);
+
+            List<Object> l = qConcurso.getResultList();
+            prefixo = "";
+            for (Object o : l) {
+                prefixo = o.toString();
 
 
-        String vsql = "select a.id_evento, a.descricao, b.inscricao, c.cargo, c.numero, c.cpf, trim(c.nome) as nome,  b.nome_cargo, b.periodo, b.id_local, d.escola, d.cidade\n" +
-                " from ensalamento a, ensalamento_candidato b, "+prefixo+"_candidato c, "+prefixo+"_local d\n" +
-                " where a.id = b.id_ensalamento and b.cpf = c.cpf and b.id_cargo = c.cargo and b.id_local = d.id\n" +
-                " and a.id_evento="+id_evento+" and b.id_local=" + idLocal +
-                " order by trim(c.nome)" ;
+                 vsql = "select a.id_evento, a.descricao, b.inscricao, c.cargo, c.numero, c.cpf, trim(c.nome) as nome,  b.nome_cargo, b.periodo, b.id_local, d.escola, d.cidade\n" +
+                        " from ensalamento a, ensalamento_candidato b, " + prefixo + "_candidato c, " + prefixo + "_local d\n" +
+                        " where a.id = b.id_ensalamento and b.cpf = c.cpf and b.id_cargo = c.cargo and b.id_local = d.id\n" +
+                        " and a.id_evento=" + id_evento + " and b.id_local=" + idLocal +
+                        " order by trim(c.nome)";
 
-        //System.out.println(vsql);
+                //System.out.println(vsql);
 
-        Query q = entityManager.createNativeQuery(vsql);
-        List<Object[]> resultList = q.getResultList();
-        for (Object[] row : resultList) {
-            EventoEnsalamentoCandidato ent = new EventoEnsalamentoCandidato();
+                Query q = entityManager.createNativeQuery(vsql);
+                List<Object[]> resultList = q.getResultList();
+                for (Object[] row : resultList) {
+                    EventoEnsalamentoCandidato ent = new EventoEnsalamentoCandidato();
 
-            ent.setId_evento(row[0] != null ? Long.parseLong(row[0].toString()) : 0L);
-            ent.setDescricao(row[1] != null ? row[1].toString() : "");
-            ent.setInscricao(row[2] != null ? row[2].toString() : "");
-            ent.setId_cargo(row[3] != null ? Long.parseLong(row[3].toString()) : 0L);
-            ent.setNumero(row[4] != null ? Long.parseLong(row[4].toString()) : 0L);
-            ent.setCpf(row[5] != null ? row[5].toString() : "");
-            ent.setNome(row[6] != null ? row[6].toString() : "");
-            ent.setCargo(row[7] != null ? row[7].toString() : "");
-            ent.setPeriodo(row[8] != null ? row[8].toString() : "");
-            ent.setId_local(row[9] != null ? Long.parseLong(row[9].toString()) : 0L);
-            ent.setEscola(row[10] != null ? row[10].toString() : "");
-            ent.setCidade(row[11] != null ? row[11].toString() : "");
+                    ent.setId_evento(row[0] != null ? Long.parseLong(row[0].toString()) : 0L);
+                    ent.setDescricao(row[1] != null ? row[1].toString() : "");
+                    ent.setInscricao(row[2] != null ? row[2].toString() : "");
+                    ent.setId_cargo(row[3] != null ? Long.parseLong(row[3].toString()) : 0L);
+                    ent.setNumero(row[4] != null ? Long.parseLong(row[4].toString()) : 0L);
+                    ent.setCpf(row[5] != null ? row[5].toString() : "");
+                    ent.setNome(row[6] != null ? row[6].toString() : "");
+                    ent.setCargo(row[7] != null ? row[7].toString() : "");
+                    ent.setPeriodo(row[8] != null ? row[8].toString() : "");
+                    ent.setId_local(row[9] != null ? Long.parseLong(row[9].toString()) : 0L);
+                    ent.setEscola(row[10] != null ? row[10].toString() : "");
+                    ent.setCidade(row[11] != null ? row[11].toString() : "");
 
-            can.add(ent);
+                    can.add(ent);
+
+                }
+            }
 
         }
 
