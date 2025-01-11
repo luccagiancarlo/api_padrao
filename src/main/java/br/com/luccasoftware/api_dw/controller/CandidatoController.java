28e62b70b08a9eeb515b39efe8cd7279ae51a617
Giancarlo Lucca <glucca@uem.br>
2025-01-11 11:32:30 -0300

    retornar candidatos por id

--- src/main/java/br/com/luccasoftware/api_dw/controller/CandidatoController.java
+++ src/main/java/br/com/luccasoftware/api_dw/controller/CandidatoController.java
@@ -46,4 +46,29 @@ public class CandidatoController {
         }
         return candidatos;
     }
+
+    @GetMapping("/retornarCandidatos/{id}")
+    public List<Candidato> retornarCandidatos(
+            @RequestHeader(value = "Authorization") String authorizationHeader,
+            @PathVariable int id) {
+
+        // Valida se o Authorization header está presente e começa com "Bearer "
+        if (!authorizationHeader.startsWith("Bearer ")) {
+            throw new RuntimeException("Token inválido.");
+        }
+
+        // O filtro JWT deve garantir que a solicitação está autenticada
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("Usuário não autenticado.");
+        }
+
+        // Retorna a lista de candidatos com o prefixo especificado
+        List<Candidato> candidatos = new ArrayList<>();
+        if (id > 0) {
+            candidatos = candidatoRepository.findAllCandidatos(id);
+        }
+        return candidatos;
+    }
+
 }
--- src/main/java/br/com/luccasoftware/api_dw/jpa/CandidatoRepository.java
+++ src/main/java/br/com/luccasoftware/api_dw/jpa/CandidatoRepository.java
@@ -124,5 +124,65 @@ public class CandidatoRepository {
 
         return candidatos;
     }
+
+
+    public List<Candidato> findAllCandidatos(int id_concurso) {
+        List<Candidato> candidatos = new ArrayList<>();
+        String sqlConcursos = "SELECT a.nome FROM concurso a where id="+id_concurso;
+        Query queryConcursos = entityManager.createNativeQuery(sqlConcursos);
+        List<Object> prefixos = queryConcursos.getResultList();
+
+        for (Object prefixo : prefixos) {
+            try {
+                String prefixoStr = prefixo.toString();
+                String sql = "SELECT a.numero as id_candidato, a.cargo as id_cargo, a.nome, a.sexo, a.nascimento, a.cep, a.cidade, a.estado, a.status, " +
+                        "a.tipo_def, a.afro, case when a.tipo_def !='NENHUMA' and a.enviou_laudo then 'sim' else 'nao' end as pcd, a.cpf,\n" +
+                        "       CASE\n" +
+                        "           WHEN a.inscricao IS NOT NULL THEN lpad(a.cargo::text, 3, '0'::text) || lpad(a.inscricao::text, 7, '0'::text)\n" +
+                        "           ELSE lpad(a.cargo::text, 3, '0'::text) || lpad(a.numero::text, 7, '0'::text)\n" +
+                        "           END as inscricao " +
+                        "FROM " + prefixoStr + "_candidato a ORDER BY a.numero";
+
+                if (!databaseUtils.existeColuna(prefixoStr + "_candidato", "afro")) {
+                    sql = "SELECT a.numero as id_candidato, a.cargo as id_cargo, a.nome, a.sexo, a.nascimento, a.cep, a.cidade, a.estado, a.status, " +
+                            "a.tipo_def, false, case when a.tipo_def !='NENHUMA' and a.enviou_laudo then 'sim' else 'nao' end as pcd, a.cpf,\n" +
+                            "       CASE" +
+                            "           WHEN a.inscricao IS NOT NULL THEN lpad(a.cargo::text, 3, '0'::text) || lpad(a.inscricao::text, 7, '0'::text)\n" +
+                            "           ELSE lpad(a.cargo::text, 3, '0'::text) || lpad(a.numero::text, 7, '0'::text)\n" +
+                            "           END as inscricao " +
+                            "FROM " + prefixoStr + "_candidato a ORDER BY a.numero";
+                }
+
+                Query query = entityManager.createNativeQuery(sql);
+                List<Object[]> resultList = query.getResultList();
+
+                for (Object[] row : resultList) {
+                    Candidato candidato = new Candidato();
+                    candidato.setPrefixo(prefixoStr);
+                    candidato.setNumero(row[0] != null ? Long.parseLong(row[0].toString()) : 0L);
+                    candidato.setId_cargo(row[1] != null ? Long.parseLong(row[1].toString()) : 0L);
+                    candidato.setNome(row[2] != null ? row[2].toString() : "");
+                    candidato.setSexo(row[3] != null ? row[3].toString() : "");
+                    candidato.setNascimento(row[4] != null ? row[4].toString() : "");
+                    candidato.setCep(row[5] != null ? row[5].toString() : "");
+                    candidato.setCidade(row[6] != null ? row[6].toString() : "");
+                    candidato.setEstado(row[7] != null ? row[7].toString() : "");
+                    candidato.setStatus(row[8] != null ? row[8].toString() : "");
+                    candidato.setTipoDef(row[9] != null ? row[9].toString() : "unknown");
+                    candidato.setAfro(row[10] != null ? Boolean.parseBoolean(row[10].toString()) : false);
+                    candidato.setPcd(row[11] != null ? Boolean.parseBoolean(row[11].toString()) : false);
+                    candidato.setCpf(row[12] != null ? row[12].toString() : "");
+                    candidato.setInscricao(row[13] != null ? row[13].toString() : "");
+                    candidatos.add(candidato);
+                }
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
+
+        return candidatos;
+    }
+
+
 }
 
